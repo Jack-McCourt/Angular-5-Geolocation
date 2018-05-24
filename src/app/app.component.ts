892afdc2d10e0c203a481d67f68df54e7ee52bd8
Jack <jack@jackonthe.net>
2018-05-24 13:18:39 +0000

    Adding initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  location:string = '';
+  locationError:string = '';
+  lat:number = 0;
+  lng:number = 0;
+  
+  constructor(private httpClient:HttpClient){  }
+  
+  getLatLng() {
+    this.httpClient.get('//www.mapquestapi.com/geocoding/v1/address?key=KFvAofpZxBngIkGTMFbntZG51telGnim&location='+encodeURI(this.location))
+    .subscribe(
+      (data:any) => {
+        if (data.results[0].locations[0].geocodeQuality === 'COUNTRY') { 
+          this.locationError = 'We couldn\'t find you :O'; 
+          this.lat = 0;
+          this.lng = 0;
+        } else {
+          this.locationError = '';
+          this.lat = data.results[0].locations[0].latLng.lat;
+          this.lng = data.results[0].locations[0].latLng.lng;
+        }
+        
+      }
+    )
+  }
 }
